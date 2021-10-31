58fedcda81a942db1842d6d0a96150d2aafd1cbe
Sangeetha <83246904+kadir12386@users.noreply.github.com>
2021-10-31 07:49:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -140,21 +140,21 @@ function Movie({ name, pic, rating, story }) {
 }
 
 // function
-function Msg({ name, image }) {
-  // const { name } = props;
-  // const { image } = props;
-  return (
-    <div>
-      <img className="profile-pic" src={image} alt={name} />
-      <h2>
-        <span role="img" aria-label="Girl">
-          👧
-        </span>
-        {name} !!!
-      </h2>
-    </div>
-  );
-}
+// function Msg({ name, image }) {
+//   // const { name } = props;
+//   // const { image } = props;
+//   return (
+//     <div>
+//       <img className="profile-pic" src={image} alt={name} />
+//       <h2>
+//         <span role="img" aria-label="Girl">
+//           👧
+//         </span>
+//         {name} !!!
+//       </h2>
+//     </div>
+//   );
+// }
 //=================================================================
 //Props - Properties
 //Custom Component
